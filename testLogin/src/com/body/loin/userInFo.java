dc53223845bed740b2a99e72c0c2094a500569e0
NewManWatch <NewManWatch@untell.com>
2019-10-28 13:35:53 +0800

    fsdf

---
+++ testLogin/src/com/body/loin/userInFo.java
@@ -0,0 +1,35 @@
+package com.body.loin;
+
+
+/*
+ *  @ autor :NewManWatch
+ *  @ time  :2019-10-1 17:05:29；
+ *  @pararm : username     the user's name
+ *            userpassword  the user's password
+ *
+ */
+
+
+
+
+public class userInFo {
+    private String username = new String();
+    private String userpassword = new String();
+
+    public String getusername() {
+        return username;
+    }
+
+    public void setusername(String username) {
+        this.username = username;
+    }
+
+    public String getuserpassword() {
+        return userpassword;
+    }
+
+    public void setuserpassword(String userpassword) {
+        this.userpassword = userpassword;
+    }
+}
+
